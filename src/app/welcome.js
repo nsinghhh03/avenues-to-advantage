a378b0c1b75dc2213fdc238cbe2132bf0573729d
pdotexe <rehanchahal08@gmail.com>
2025-07-02 19:50:26 -0400

    initial commit

---
+++ src/app/welcome.js
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import styles from "./welcome.module.css";
+
+export default function Welcome() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <div className={styles.welcomeContainer}>
+          <div className={styles.globeImageWrapper}>
+            <Image
+              src="/children-globe.png"
+              alt="Children standing on a globe"
+              fill
+              className={styles.globeImage}
+              priority
+              sizes="(max-width: 400px) 80vw, 300px"
+            />
+          </div>
+          <div className={styles.bubble}>
+            <h1 className={styles.title}>AVENUES TO ADVANTAGE</h1>
+          </div>
+          <div className={styles.subtitleBox}>
+            <p className={styles.subtitle}>
+              Explore how everyday life<br />shapes opportunity!
+            </p>
+          </div>
+          <div className={styles.arrowContainer}>
+            <span className={styles.downArrow}>↓</span>
+          </div>
+          <button className={styles.startButton}>Get started</button>
+        </div>
+      </main>
+    </div>
+  );
+} 
