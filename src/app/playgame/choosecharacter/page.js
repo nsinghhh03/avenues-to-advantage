85da061e9203893910195286c07a05878cbff45b
pdotexe <rehanchahal08@gmail.com>
2025-07-13 23:04:37 -0400

    new loading page

--- src/app/playgame/choosecharacter/page.js
+++ src/app/playgame/choosecharacter/page.js
@@ -3,11 +3,9 @@ import { Suspense } from "react";
 import ChooseCharacterClient from "./ChooseCharacterClient";
 
 export default function ChooseCharacterPage() {
-  return ( 
-    <div style={{ maxHeight: '90vh', overflowY: 'auto', textAlign: 'center', fontSize: '1.2rem', fontWeight: 'bold', }}>
+  return (
     <Suspense fallback={<div>Spin The Wheel Before Proceeding!</div>}>
       <ChooseCharacterClient />
     </Suspense>
-    </div>
   );
 } 
